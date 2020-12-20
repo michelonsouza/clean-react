130eff4d8c759b30d54efbf7b1f5464c0143756a
Michelon Souza <michelon.souza@hotmail.com>
2020-12-20 07:01:52 -0300

    feat: ensure AxiosHttpClient with correct body

--- src/infra/http/axios-http-client/axios-http-client.ts
+++ src/infra/http/axios-http-client/axios-http-client.ts
@@ -2,7 +2,7 @@ import { HttpPostParams } from '@/data/protocols/http/http-post-client';
 import axios from 'axios';
 
 export class AxiosHttpClient {
-  async post({ url }: HttpPostParams<any>): Promise<void> {
-    await axios.post(url);
+  async post({ url, body }: HttpPostParams<any>): Promise<void> {
+    await axios.post(url, body);
   }
 }

bdc8de4c9b42d6ba32b8d7eac9b3bce81c313abf
Michelon Souza <michelon.souza@hotmail.com>
2020-12-20 06:44:33 -0300

    feat: ensure AxiosHttpClient calls axios with correct URL

---
+++ src/infra/http/axios-http-client/axios-http-client.ts
@@ -0,0 +1,8 @@
+import { HttpPostParams } from '@/data/protocols/http/http-post-client';
+import axios from 'axios';
+
+export class AxiosHttpClient {
+  async post({ url }: HttpPostParams<any>): Promise<void> {
+    await axios(url);
+  }
+}

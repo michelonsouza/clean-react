33dd96db749e7f5472655e453b4437a4e37a1241
Michelon Souza <michelon.souza@hotmail.com>
2020-12-25 22:20:57 -0300

    refactor: add index file in pages

--- src/main/index.tsx
+++ src/main/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import Login from '@/presentation/pages/Login';
+import { Login } from '@/presentation/pages';
 
 ReactDOM.render(
   <React.StrictMode>

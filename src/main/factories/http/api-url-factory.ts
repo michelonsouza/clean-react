6491e613ba744e6040e1b55a54bf6801646568c5
Michelon <michelon.souza@hotmail.com>
2020-12-29 06:11:15 -0300

    feat: add dotenv file

--- src/main/factories/http/api-url-factory.ts
+++ src/main/factories/http/api-url-factory.ts
@@ -1,3 +1,3 @@
 export const makeApiUrl = (path: string): string => {
-  return `https://fordevs.herokuapp.com/api${path}`;
+  return `${process.env.API_URL}${path}`;
 };
--- webpack.config.js
+++ webpack.config.js
@@ -1,64 +1,82 @@
-const {resolve} = require('path');
+const fs = require('fs');
+const { resolve } = require('path');
+const { DefinePlugin } = require('webpack');
+const dotenv = require('dotenv');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
 
-module.exports = {
-  mode: 'development',
-  entry: './src/main/index.tsx',
-  devtool: 'inline-source-map',
-  output: {
-    path: resolve(__dirname, 'public', 'js'),
-    publicPath: resolve(__dirname, 'public', 'js'),
-    filename: 'bundle.js',
-  },
-  resolve: {
-    extensions: ['.ts', '.tsx', '.js', '.scss'],
-    plugins: [
-      new TsconfigPathsPlugin({
-        configFile: resolve(__dirname, 'tsconfig.json'),
-      }),
-    ]
-  },
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        loader: 'ts-loader',
-        exclude: /node_modules/,
-      },
-      {
-        test: /\.scss$/,
-        use: [
-          {
-            loader: 'style-loader',
-          },
-          {
-            loader: 'css-loader',
-            options: {
-              modules: true,
+const getEnvironment = (env) => {
+  const basePath = resolve(__dirname, '.env');
+  const envPath = `${basePath}.${env.NODE_ENV}`;
+  const finalPath = fs.existsSync(envPath) ? envPath : basePath;
+  const fileEnv = dotenv.config({ path: finalPath }).parsed;
+
+  return Object.entries(fileEnv).reduce((accumulator, [key, value]) => {
+    accumulator[`process.env.${key}`] = JSON.stringify(value);
+
+    return accumulator;
+  }, {});
+}
+
+module.exports = env => {
+  const environment = getEnvironment(env);
+
+  return {
+    mode: 'development',
+    entry: './src/main/index.tsx',
+    devtool: 'inline-source-map',
+    output: {
+      path: resolve(__dirname, 'public', 'js'),
+      publicPath: resolve(__dirname, 'public', 'js'),
+      filename: 'bundle.js',
+    },
+    resolve: {
+      extensions: ['.ts', '.tsx', '.js', '.scss'],
+      plugins: [
+        new TsconfigPathsPlugin({
+          configFile: resolve(__dirname, 'tsconfig.json'),
+        }),
+      ],
+    },
+    module: {
+      rules: [
+        {
+          test: /\.tsx?$/,
+          loader: 'ts-loader',
+          exclude: /node_modules/,
+        },
+        {
+          test: /\.scss$/,
+          use: [
+            {
+              loader: 'style-loader',
             },
-          },
-          {
-            loader: 'sass-loader',
-          },
-        ]
-      }
-    ]
-  },
-  devServer: {
-    contentBase: resolve(__dirname, 'public'),
-    writeToDisk: true,
-    compress: true,
-    historyApiFallback: true,
-    port: 3000,
-    open: true,
-    liveReload: true,
-  },
-  externals: {
-    react: 'React',
-    'react-dom': 'ReactDOM',
-  },
-  plugins: [
-    new CleanWebpackPlugin(),
-  ],
+            {
+              loader: 'css-loader',
+              options: {
+                modules: true,
+              },
+            },
+            {
+              loader: 'sass-loader',
+            },
+          ],
+        },
+      ],
+    },
+    devServer: {
+      contentBase: resolve(__dirname, 'public'),
+      writeToDisk: true,
+      compress: true,
+      historyApiFallback: true,
+      port: 3000,
+      open: true,
+      liveReload: true,
+    },
+    externals: {
+      react: 'React',
+      'react-dom': 'ReactDOM',
+    },
+    plugins: [new CleanWebpackPlugin(), new DefinePlugin(environment)],
+  };
 };

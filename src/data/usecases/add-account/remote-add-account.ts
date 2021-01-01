38dad30bee8709c0171db666c70265cf9992bb8f
Michelon Souza <michelon.souza@hotmail.com>
2020-12-31 23:22:45 -0300

    feat: ensure RemoteAddAccount calls HttpPostClient with correct url

---
+++ src/data/usecases/add-account/remote-add-account.ts
@@ -0,0 +1,28 @@
+import { UnexpectedError } from '@/domain/errors';
+import { AddAccount, AddAccountParams } from '@/domain/usecases';
+import { AccountModel } from '@/domain/models';
+import { HttpPostClient, HttpStatusCode } from '@/data/protocols/http';
+
+export class RemoteAddAccount implements AddAccount {
+  constructor(
+    private readonly url: string,
+    private readonly httpPostClient: HttpPostClient<
+      AddAccountParams,
+      AccountModel
+    >,
+  ) {}
+
+  async add(params: AddAccountParams): Promise<AccountModel> {
+    const httpResponse = await this.httpPostClient.post({
+      url: this.url,
+      body: params,
+    });
+
+    switch (httpResponse.statusCode) {
+      case HttpStatusCode.ok:
+        return httpResponse.body as AccountModel;
+      default:
+        throw new UnexpectedError();
+    }
+  }
+}

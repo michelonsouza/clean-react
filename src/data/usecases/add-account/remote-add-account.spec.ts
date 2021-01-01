e99b88a8635fd412906cc9a185cb77cd6577606e
Michelon Souza <michelon.souza@hotmail.com>
2020-12-31 23:23:19 -0300

    test: ensure RemoteAddAccount calls HttpPostClient with correct url

---
+++ src/data/usecases/add-account/remote-add-account.spec.ts
@@ -0,0 +1,42 @@
+import faker from 'faker';
+
+import { AccountModel } from '@/domain/models';
+import { AddAccountParams } from '@/domain/usecases';
+// import { InvalidCredentialsError, UnexpectedError } from '@/domain/errors';
+// import { mockAddAccountParams, mockAccountModel } from '@/domain/mocks';
+import { mockAddAccountParams } from '@/domain/mocks';
+import { HttpPostClientSpy } from '@/data/mocks';
+// import { HttpStatusCode } from '@/data/protocols/http';
+
+import { RemoteAddAccount } from './remote-add-account';
+
+type SutTypes = {
+  sut: RemoteAddAccount;
+  httpPostClientSpy: HttpPostClientSpy<AddAccountParams, AccountModel>;
+};
+
+const makeSut = (url = faker.internet.url()): SutTypes => {
+  const httpPostClientSpy = new HttpPostClientSpy<
+    AddAccountParams,
+    AccountModel
+  >();
+  const sut = new RemoteAddAccount(url, httpPostClientSpy);
+
+  return {
+    sut,
+    httpPostClientSpy,
+  };
+};
+
+describe('RemoteAddAccount', () => {
+  it('should call HttpPostClient with correct url', async () => {
+    const url = faker.internet.url();
+    const { sut, httpPostClientSpy } = makeSut(url);
+
+    await sut.add(mockAddAccountParams());
+
+    expect(httpPostClientSpy.url).toBe(url);
+  });
+});
+
+export default {};
